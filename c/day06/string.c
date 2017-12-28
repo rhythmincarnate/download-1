bb09fc389af3a596e1dc7f800ac9c2076e37d354
lilei <lilei@11>
2017-12-28 16:17:09 +0800

    c day06

---
+++ c/day06/string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+
+
+int main(void)
+{
+
+
+	return 0;
+}
+
+int mystrlen(const char *p)
+{
+
+
+}
+
+char *mystrcpy(char *dest, const char *src)
+{
+
+}
+
+
+char *mystrcat(char *dest, const char *src)
+{
+
+
+}
+
+int mystrcmp(const char *s1, const char *s2)
+{
+
+
+}
+
